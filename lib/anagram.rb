7eb9217aa7fb3dcc36340b18ba2aa42506d9047d
Mark Momot <markmomot03@gmail.com>
2022-02-07 20:08:55 -0500

    commit tree 8fca87927579c7eac240f5ef50fc9b03c6efbcf2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(ary)
+        words_ary = []
+        word_ary = @word.chars
+
+        ary.each {|word| 
+            if word_ary.sort == word.chars.sort
+                words_ary.push(word)
+            end
+        }
+
+        words_ary
+    end
+end
